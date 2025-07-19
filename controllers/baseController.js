49713505e95ee323f100f9c9457b66f9de392e28
steven_savarin <srssdesing@gmail.com>
2025-07-19 22:20:04 +0200

    task 02 and 03

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -6,5 +6,10 @@ baseController.buildHome = async function(req, res){
   res.render("index", {title: "Home", nav})
 }
 
+// Intentional error trigger
+baseController.triggerError = function (req, res, next) {
+  next({ status: 500, message: "Intentional server error! I did it on purpose." })
+}
+
 module.exports = baseController
 
--- server.js
+++ server.js
@@ -6,41 +6,55 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
-const expressLayouts = require("express-ejs-layouts")
 const static = require("./routes/static")
-const inventoryRoute = require("./routes/inventoryRoute")
 const baseController = require("./controllers/baseController")
-const utilities = require("./utilities/")
+const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/index")
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
  *************************/
-app.use(express.static("public"))
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout")
+app.use(static)
 // Index route
-app.get("/", baseController.buildHome);
+app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", inventoryRoute)
+//Intentional Error Route
+app.get("/error-test", utilities.handleErrors(baseController.triggerError))
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
   next({status: 404, message: 'Sorry, we appear to have lost that page.'})
 })
 
-
 /* ***********************
- * Express Error Handler
- * Place after all other middleware
- *************************/
+* Express Error Handler
+* Place after all other middleware
+*************************/
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  let message
+  if (err.message) {
+    message = err.message
+  } else if (err.status == 404) {
+    message = 'Sorry, we appear to have lost that page.'
+  } else {
+    message = 'Oh no! There was a crash. Maybe try a different route?'
+  }
   res.render("errors/error", {
     title: err.status || 'Server Error',
-    message: err.message,
+    message,
     nav
   })
 })
@@ -49,8 +63,8 @@ app.use(async (err, req, res, next) => {
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT || 5500
-const host = process.env.HOST || "localhost"
+const port = process.env.PORT
+const host = process.env.HOST
 
 /* ***********************
  * Log statement to confirm server operation
